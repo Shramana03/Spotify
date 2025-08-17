293cb510c013c58f2826b4509e315160c41a866a
Shramana03 <shramanabhattacharya2003@gmail.com>
2025-08-17 22:15:41 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,44 @@
+console.log("Welcome to spotify");
+//Initialize the variables
+let songIndex=0;
+let audioElement = new Audio('1.mp3');
+let masterPlay= document.getElementById('masterPlay');
+let myProgressBar=document.getElementById('myProgressBar');
+let gif=document.getElementById('gif');
+
+
+let songs=[
+    {songName: "Salam-e-Ishk",filePath:"song/1.mp3",coverPath: "covers/1.jpg"},
+    {songName: "Salam-e-Ishk",filePath:"song/1.mp3",coverPath: "covers/1.jpg"},
+    {songName: "Salam-e-Ishk",filePath:"song/1.mp3",coverPath: "covers/1.jpg"},
+    {songName: "Salam-e-Ishk",filePath:"song/1.mp3",coverPath: "covers/1.jpg"},
+    {songName: "Salam-e-Ishk",filePath:"song/1.mp3",coverPath: "covers/1.jpg"},
+    {songName: "Salam-e-Ishk",filePath:"song/1.mp3",coverPath: "covers/1.jpg"}
+]
+// audioElement.play();
+
+// Handle play/pause click
+masterPlay.addEventListener('click', ()=>{
+    if(audioElement.paused || audioElement.currentTime<=0){
+        audioElement.play();
+        masterPlay.classList.remove('fa-play-circle');
+        masterPlay.classList.add('fa-pause-circle');
+        gif.style.opacity=1;
+
+    }
+    else{
+        audioElement.pause();
+        masterPlay.classList.remove('fa-pause-circle');
+        masterPlay.classList.add('fa-play-circle');
+        gif.styple.opacity=0;
+    }
+})
+
+//Listen to Events
+audioElement.addEventListener('timeupdate', ()=>{
+    console.log('timeupdate');
+    // Update Seekbar
+    progress= parseInt((audioElement.currentTime/audioElement.duration)*100);
+    console.log(progress);
+    myProgressBar.value=progress;
+})
